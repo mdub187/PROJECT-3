baf4aa65f65e986e457d54269759385f6cdad6d4
aawllms <aarondwilliams8@gmail.com>
2025-03-03 17:34:05 -0700

    update for files after testing

--- server/dist/schemas/resolvers.js
+++ server/dist/schemas/resolvers.js
@@ -1,3 +1,191 @@
+// import { signToken } from "../services/auth.js";
+// import { AuthenticationError } from "../services/auth.js";
+// import { User, Resource } from "../models/index.js";
+// import { UserDocument } from "../models/User.js";
+// const resolvers = {
+//   Query: {
+//     getSingleUser: async (_parent: any, _args: any, context: any) => {
+//       const foundUser = await User.findOne({
+//         username: context.user.username,
+//       });
+//       if (!foundUser) {
+//         throw new AuthenticationError("Authentication Error");
+//       }
+//       return foundUser;
+//     },
+//     getResource: async (_parent: any, { resourceId }: any, context: any) => {
+//     getAllUsers: async (_parent: any, _args: any, context: any) => {
+//       if (context?.user) {
+//         const allUsers = await User.find();
+//         return allUsers;
+//       } else {
+//         throw new AuthenticationError("Authentication Error");
+//       }
+//     },
+//   }
+//     getUserByUsername: async (_parent: any, args: any, context: any) => {
+//       if (context?.user) {
+//         const foundUser = await User.findOne({
+//           username: args.username,
+//         });
+//         if (!foundUser) {
+//           throw new AuthenticationError("User not found");
+//         }
+//         return foundUser;
+//       } else {
+//         throw new AuthenticationError("Authentication Error");
+//       }
+//     },
+//     getResource: async (_parent: any, { _id }: any, context: any) => {
+//       if (!context.user) {
+//         throw new AuthenticationError("You need to be logged in!");
+//       }
+//       try {
+//         const resource = await Resource.findById(resourceId);
+//         if (!resource) {
+//           throw new Error("Resource not found");
+//         }
+//         return resource;
+//       } catch (error) {
+//         throw new Error("Error fetching resource");
+//       }
+//     },
+//     getAllResources: async (_parent: any, _args: any) => {
+//       const getResources = await Resource.find();
+//       return getResources;
+//     },
+//     //getResourceByCategory()
+//   },
+// },
+//   Mutation: {
+//     createUser: async (_parent: any, args: any, _context: any) => {
+//       const user = await User.create(args);
+//       if (!user) {
+//         return null;
+//       }
+//       const token = signToken(user.username, user.password, user._id);
+//       return { token, user };
+//     },
+//     updateUser: async (_parent: any, args: any, context: any) => {
+//       if (context.user) {
+//         // const updatedUser = await User.findByIdAndUpdate(
+//         //   context.user._id,
+//         //   {
+//         //     $set: {
+//         //       //   ...args,
+//         //       username: args.username || context.user.username,
+//         //       email: args.email || context.user.email,
+//         //       password: args.password || context.user.password,
+//         //     },
+//         //   },
+//         //   {
+//         //     new: true,
+//         //     runValidators: true,
+//         //   }
+//         // );
+//         const targetUser: UserDocument | null = await User.findById(
+//           context.user._id
+//         );
+//         if (args?.username && targetUser?.username) {
+//           targetUser.username = args.username;
+//         }
+//         if (args?.email && targetUser?.email) {
+//           targetUser.email = args.email;
+//         }
+//         if (args?.password && targetUser?.password) {
+//           targetUser.password = args.password;
+//         }
+//         targetUser?.save();
+//         console.log(targetUser);
+//         return targetUser;
+//       }
+//       throw new AuthenticationError("Authentication Error");
+//     },
+//     deleteUser: async (_parent: any, _args: any, context: any) => {
+//       console.log(context.user);
+//       if (context.user) {
+//         const user = await User.findByIdAndDelete(context.user._id);
+//         console.log(user);
+//         return;
+//       }
+//       throw new AuthenticationError("Must be logged in to delete account");
+//     },
+//     login: async (_parent: any, args: any, _context: any) => {
+//       const user = await User.findOne({
+//         $or: [{ username: args.username }, { email: args.email }],
+//       });
+//       if (!user) {
+//         throw new AuthenticationError("Authentication Error");
+//       }
+//       const correctPw = await user.isCorrectPassword(args.password);
+//       if (!correctPw) {
+//         throw new AuthenticationError("Authentication Error");
+//       }
+//       const token = signToken(user.username, user.email, user._id);
+//       return { token, user };
+//     },
+//     createResource: async (
+//       _parent: any,
+//       { title, description, url }: any,
+//       context: any
+//     ) => {
+//       if (!context.user) {
+//         throw new AuthenticationError("You need to be logged in!");
+//       }
+//       try {
+//         const newResource = new Resource({
+//           title,
+//           description,
+//           url,
+//         });
+//         const savedResource = await newResource.save();
+//         return savedResource;
+//       } catch (error) {
+//         throw new Error("Error adding resource");
+//       }
+//     },
+//     deleteResource: async (_parent: any, { resourceId }: any, context: any) => {
+//       if (!context.user) {
+//         throw new AuthenticationError("You need to be logged in!");
+//       }
+//       try {
+//         const resource = await Resource.findByIdAndDelete(resourceId);
+//         if (!resource) {
+//           throw new Error("Resource not found");
+//         }
+//         console.log(`Successfully deleted resource with ID: ${resourceId}`);
+//         return resource;
+//       } catch (error) {
+//         throw new Error("Error deleting resource");
+//       }
+//     },
+//     // updateResource: {},
+//     saveResource: async (_parent: any, args: any, context: any) => {
+//       try {
+//         const updatedUser = await User.findByIdAndUpdate(
+//           { _id: context.user._id },
+//           { $addToSet: { savedResource: args.resourceId } },
+//           { new: true, runValidators: true }
+//         );
+//         return updatedUser;
+//       } catch (err) {
+//         return null;
+//       }
+//     },
+//     removeResource: async (_parent: any, args: any, context: any) => {
+//       const updatedUser = await User.findByIdAndUpdate(
+//         { _id: context.user._id },
+//         { $pull: { savedResource: { resourceId: args.resourceId } } },
+//         { new: true }
+//       );
+//       if (!updatedUser) {
+//         return null;
+//       }
+//       return updatedUser;
+//     },
+//   },
+// };
+// export default resolvers;
 import { signToken } from "../services/auth.js";
 import { AuthenticationError } from "../services/auth.js";
 import { User, Resource } from "../models/index.js";
@@ -12,7 +200,6 @@ const resolvers = {
             }
             return foundUser;
         },
-<<<<<<< HEAD
         getAllUsers: async (_parent, _args, context) => {
             if (context?.user) {
                 const allUsers = await User.find();
@@ -28,7 +215,7 @@ const resolvers = {
                     username: args.username,
                 });
                 if (!foundUser) {
-                    throw new AuthenticationError("AUser not found");
+                    throw new AuthenticationError("User not found");
                 }
                 return foundUser;
             }
@@ -36,14 +223,12 @@ const resolvers = {
                 throw new AuthenticationError("Authentication Error");
             }
         },
-        // getResource: {},
-=======
-        getResource: async (_parent, { _id }, context) => {
+        getResource: async (_parent, { resourceId }, context) => {
             if (!context.user) {
                 throw new AuthenticationError("You need to be logged in!");
             }
             try {
-                const resource = await Resource.findById(_id);
+                const resource = await Resource.findById(resourceId);
                 if (!resource) {
                     throw new Error("Resource not found");
                 }
@@ -53,7 +238,11 @@ const resolvers = {
                 throw new Error("Error fetching resource");
             }
         },
->>>>>>> main
+        // getAllResources: async (_parent: any, _args: any) => {
+        //   const getResources = await Resource.find();
+        //   return getResources;
+        // },
+        //getResourceByCategory()
     },
     Mutation: {
         createUser: async (_parent, args, _context) => {
@@ -131,35 +320,52 @@ const resolvers = {
                     url,
                 });
                 const savedResource = await newResource.save();
-                // console.log(
-                //   `Successfully added resource with ID: ${savedResource._id}`
-                // );
                 return savedResource;
             }
             catch (error) {
                 throw new Error("Error adding resource");
             }
         },
-        deleteResource: async (_parent, { _id }, context) => {
+        deleteResource: async (_parent, { resourceId }, context) => {
             if (!context.user) {
                 throw new AuthenticationError("You need to be logged in!");
             }
-            // console.log(`Attempting to delete resource with ID: ${_id}`);
             try {
-                const resource = await Resource.findByIdAndDelete(_id);
+                const resource = await Resource.findByIdAndDelete(resourceId);
                 if (!resource) {
-                    // console.log(`Resource with ID: ${_id} not found`);
                     throw new Error("Resource not found");
                 }
-                console.log(`Successfully deleted resource with ID: ${_id}`);
+                console.log(`Successfully deleted resource with ID: ${resourceId}`);
                 return resource;
             }
             catch (error) {
-                // console.error(`Error deleting resource with ID: ${_id}`, error);
                 throw new Error("Error deleting resource");
             }
         },
         // updateResource: {},
+        // saveResource: async (_parent: any, args: any, context: any) => {
+        //   try {
+        //     const updatedUser = await User.findByIdAndUpdate(
+        //       { _id: context.user._id },
+        //       { $addToSet: { savedResource: args.resourceId } },
+        //       { new: true, runValidators: true }
+        //     );
+        //     return updatedUser;
+        //   } catch (err) {
+        //     return null;
+        //   }
+        // },
+        // removeResource: async (_parent: any, args: any, context: any) => {
+        //   const updatedUser = await User.findByIdAndUpdate(
+        //     { _id: context.user._id },
+        //     { $pull: { savedResource: { resourceId: args.resourceId } } },
+        //     { new: true }
+        //   );
+        //   if (!updatedUser) {
+        //     return null;
+        //   }
+        //   return updatedUser;
+        // },
     },
 };
 export default resolvers;
