65d8294c0b48e6f9f6658568907e1bdc8d34cb6b
aawllms <aarondwilliams8@gmail.com>
2025-03-05 11:19:21 -0700

    updated code to correct login issues

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -7,7 +7,7 @@ export const LOGIN_MUTATION = gql`
     }
   }
 `;
-
+//comment
 export const CREATE_USER = gql`
   mutation CreateUser($username: String!, $email: String!, $password: String!) {
     createUser(username: $username, email: $email, password: $password) {
